86ae3532b53d6a444cac0cbca48f38b369a1a8f3
Jose Torres <jatorr98@gmail.com>
2019-11-23 15:43:51 -0600

    added webView

--- TeamFestive/TeamFestive/ViewController.swift
+++ TeamFestive/TeamFestive/ViewController.swift
@@ -7,17 +7,18 @@
 //
 
 import UIKit
+import WebKit
 //import OSLog
-class ViewController: UIViewController{
+class ViewController: UIViewController, WKNavigationDelegate{
 
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var venueLabel: UILabel!
-    @IBOutlet weak var descLabel: UITextView!
-    @IBOutlet weak var siteLabel: UILabel!
     
+    
+    @IBOutlet weak var webView: WKWebView!
     @IBOutlet weak var saveButton: UIBarButtonItem!
     
     var date:String!;
@@ -31,15 +32,36 @@ class ViewController: UIViewController{
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.dateLabel.text = date
-        self.descLabel.text = desc
         self.image.image = img
         self.nameLabel.text = name
         self.priceLabel.text = price
-        self.siteLabel.text = site
         self.venueLabel.text = venue
-        
-        
-    }
+        self.webView.navigationDelegate = self
+        let request = URLRequest(url: URL(string: self.site)!)
+        self.webView.load(request)
+         
+     }
+     func webView(_ webView: WKWebView, didReceiveServerRedirectForProvisionalNavigation navigation: WKNavigation!) {
+         print("webView:\(webView) didReceiveServerRedirectForProvisionalNavigation:\(String(describing: navigation))")
+         print(webView.url!)
+         self.webView.load( URLRequest(url: webView.url!))
+     }
+     func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+         print("didCommitNavigation - content arriving?")
+     }
+
+     private func webView(webView: WKWebView, didFailNavigation navigation: WKNavigation!, withError error: NSError) {
+         print("didFailNavigation")
+     }
+
+     func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+         print("didStartProvisionalNavigation \(String(describing: navigation))")
+     }
+
+     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+         print("didFinishNavigation")
+         
+     }
     //MARK: ADD THE PREPARE CODE (Check for changes in the above code... i don't remember if i made any changes)
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
@@ -52,7 +74,7 @@ class ViewController: UIViewController{
         let vc = segue.destination as? FestivalListTableViewController
         vc?.tableView.reloadData()
         let imgData = self.image.image!.jpegData(compressionQuality: 1.0)
-        vc?.save(name: self.nameLabel.text!, desc: self.descLabel.text!, image:imgData!, date: self.dateLabel.text!, price: self.priceLabel.text!, venue: self.venueLabel.text!, website: self.siteLabel.text!)
+        vc?.save(name: self.nameLabel.text!, desc: self.desc, image:imgData!, date: self.dateLabel.text!, price: self.priceLabel.text!, venue: self.venueLabel.text!, website: self.site)
         
     }
     
